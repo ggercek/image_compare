71c0b5a667c4dc583fc8b70ee93711522a26a739
Gurcan Gercek <gurcangercek@gmail.com>
2019-08-15 12:58:45 -0400

    FilePair model added

---
+++ tests/test_models.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `models` module."""
+
+
+import unittest
+from image_compare import models
+
+
+class TestModelFilePair(unittest.TestCase):
+    """Tests for `FilePair` class."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_initial_values(self):
+        fp = models.FilePair("aa.png", "bb.png")
+        assert fp.file1 == "aa.png"
+        assert fp.file2 == "bb.png"
+        assert fp.similarity == -1.0
+        assert fp.elapsed == -1.0
+        assert fp.line_num == -1
+        assert fp.skipped is False
