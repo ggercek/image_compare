2e31036ff2235120362b90abea4c5ae3f2dede52
Gurcan Gercek <gurcangercek@gmail.com>
2019-08-16 13:54:20 -0400

    Refactored Similarity module
    - Classes are replaced with methods.
    - Register decorator added for easy access to methods outside the module
    - Code level documentation added
    - Unit tests updated

--- image_compare/similarity.py
+++ image_compare/similarity.py
@@ -2,6 +2,8 @@
 """This module contains the wrapper classes for scikit-image library methods"""
 
 import time
+from functools import wraps
+from collections import defaultdict
 from skimage import io, img_as_float
 from skimage.measure import compare_ssim as ssim
 from skimage.measure import compare_nrmse as nrmse
@@ -10,26 +12,71 @@ from skimage.measure import compare_mse as mse
 from image_compare.exceptions import FileError, ArgumentError
 
 
-# TODO: move this to an absctract class
+MEASUREMENTS = defaultdict(None)
+
+
+def get_similarity_measurement(name):
+    """ Returns similarity measurement method
+    :param name: name of the method ["ssim", "nrmse", "mse"]
+    :return: similarity method if the name is valid
+    :raises:
+        ArgumentError: if name is a not a valid implemented method
+    """
+    method = MEASUREMENTS.get(name)
+    if method is None:
+        raise ArgumentError(f"{name} is a not a supported similarity method")
+
+    return method
+
+
+def register(name=""):
+    """Registers the similarity method with given name to MEASUREMENTS dictionary
+
+    :param name: name of the method e.g ssim, nrmse, mse, etc.
+    :return: decorated method
+    """
+    def decorator_register(func):
+        """Decorator to register similarity measurments"""
+        MEASUREMENTS[name] = func
+    return decorator_register
+
+
 def time_similarity_calculation_and_update_pair(method, timing_method=time.process_time):
-    """This decorator will measure the time of the execution
-     of decorated function and update the pair parameter pass to that function.
-     Process_time excludes time elapsed during sleep, if sleep time is important time.perf_counter() can be used.
+    """Measures the time of the execution of decorated function and
+    update the pair parameter pass to that function.
+
+    :param method: similarity method to decorate
+    :param timing_method: timing method to use, default value is time.process_time
+    Process_time excludes time elapsed during sleep, if sleep time is important time.perf_counter() can be used.
      See the documentation;
       * process_time: https://docs.python.org/3/library/time.html#time.process_time
       * perf_counter: https://docs.python.org/3/library/time.html#time.perf_counter
+    :return: the result of decorated function
     """
+    @wraps(method)
     def timed(*args, **kw):
         ts = timing_method()
         result = method(*args, **kw)
         te = timing_method()
-        # Skip the first argument that is object
-        args[1].elapsed = te - ts
+        # Update pair's elapsed time
+        args[0].elapsed = te - ts
         return result
     return timed
 
 
-def check_files_and_open(pair, same_size_enforce=True):
+def __check_files_and_open(pair, same_size_enforce=True):
+    """Private function that enforces common checks and returns the file handlers
+
+    :param pair: `FilePair` object
+    :param same_size_enforce: enables dimension check of image pairs
+    :return:
+        image1, image2 : ndarray, ndarray
+        loaded images
+    :raises:
+        FileError: if an image is missing
+        ArgumentError: if same_size_enforce=True and images have different dimensions
+    """
+
     image1 = None
     image2 = None
 
@@ -52,35 +99,47 @@ def check_files_and_open(pair, same_size_enforce=True):
     return image1, image2
 
 
-class StructuralSimilarityIndexMeasure:
-    """Compute the mean structural similarity index between two images."""
+@register(name="ssim")
+@time_similarity_calculation_and_update_pair
+def calculate_ssmi_similarity(pair):
+    """Compute the mean structural similarity index between two images.
 
-    @time_similarity_calculation_and_update_pair
-    def calculate_similarity(self, pair, coloredImages=True):
-        image1, image2 = check_files_and_open(pair)
-        img1f = img_as_float(image1)
-        img2f = img_as_float(image2)
-        similarity = ssim(img1f, img2f, multichannel=True)
-        pair.similarity = round(1 - similarity, 3)
-
-
-class NormalizedRootMeanSquaredErrorMeasure:
-    """Compute the normalized root mean-squared error (NRMSE) between two images."""
-
-    @time_similarity_calculation_and_update_pair
-    def calculate_similarity(self, pair):
-        image1, image2 = check_files_and_open(pair)
-        img1f = img_as_float(image1)
-        img2f = img_as_float(image2)
-        similarity = nrmse(img1f, img2f)
-        pair.similarity = round(similarity, 3)
+    :param pair: image pair to compare
+    :return: similarity : float
+        the similarity value of given image pair
+    """
+    image1, image2 = __check_files_and_open(pair)
+    img1f = img_as_float(image1)
+    img2f = img_as_float(image2)
+    similarity = ssim(img1f, img2f, multichannel=True)
+    pair.similarity = round(1 - similarity, 3)
 
 
-class MeanSquaredErrorMeasure:
-    """Compute the mean-squared error between two images."""
+@register(name="nrmse")
+@time_similarity_calculation_and_update_pair
+def calculate_nrmse_similarity(pair):
+    """Compute the normalized root mean-squared error (NRMSE) between two images.
 
-    @time_similarity_calculation_and_update_pair
-    def calculate_similarity(self, pair):
-        image1, image2 = check_files_and_open(pair)
-        similarity = mse(image1, image2)
-        pair.similarity = round(similarity / float(image1.shape[0] * image1.shape[1]), 3)
+    :param pair: image pair to compare
+    :return: similarity : float
+        the similarity value of given image pair
+    """
+    image1, image2 = __check_files_and_open(pair)
+    img1f = img_as_float(image1)
+    img2f = img_as_float(image2)
+    similarity = nrmse(img1f, img2f)
+    pair.similarity = round(similarity, 3)
+
+
+@register(name="mse")
+@time_similarity_calculation_and_update_pair
+def calculate_mse_similarity(pair):
+    """Compute the mean-squared error between two images.
+
+    :param pair: image pair to compare
+    :return: similarity : float
+        the similarity value of given image pair
+    """
+    image1, image2 = __check_files_and_open(pair)
+    similarity = mse(image1, image2)
+    pair.similarity = round(similarity / float(image1.shape[0] * image1.shape[1]), 3)
--- tests/test_similarity.py
+++ tests/test_similarity.py
@@ -5,96 +5,87 @@
 import unittest
 from image_compare.models import FilePair
 from image_compare.exceptions import FileError, ArgumentError
-from image_compare.similarity import StructuralSimilarityIndexMeasure, NormalizedRootMeanSquaredErrorMeasure, MeanSquaredErrorMeasure
+from image_compare.similarity import get_similarity_measurement
 
 class TestSSMISimilarity(unittest.TestCase):
     """Tests for `SSIM` class."""
 
     def setUp(self):
         """Set up test fixtures, if any."""
+        self.ssim = get_similarity_measurement("ssim")
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
     def test_ssim_missing_file_first_argument(self):
         pair = FilePair("no_such_file_exists.png", "files/tests/images/0-0-white.png")
-        ssim = StructuralSimilarityIndexMeasure()
         with self.assertRaises(FileError):
-            ssim.calculate_similarity(pair)
+            self.ssim(pair)
         assert pair.skipped is True
         assert pair.similarity == -1.0
         assert pair.elapsed == -1.0
 
     def test_ssim_missing_file_second_argument(self):
         pair = FilePair("files/tests/images/0-0-white.png", "no_such_file_exists.png")
-        ssim = StructuralSimilarityIndexMeasure()
         with self.assertRaises(FileError):
-            ssim.calculate_similarity(pair)
+            self.ssim(pair)
         assert pair.skipped is True
         assert pair.similarity == -1.0
         assert pair.elapsed == -1.0
 
     def test_ssim_files_with_different_sizes(self):
         pair = FilePair("files/tests/images/1-0-small-white.png", "files/tests/images/1-1-big-white.png")
-        ssim = StructuralSimilarityIndexMeasure()
         with self.assertRaises(ArgumentError):
-            ssim.calculate_similarity(pair)
+            self.ssim(pair)
         assert pair.skipped is True
         assert pair.similarity == -1.0
         assert pair.elapsed == -1.0
 
     def test_ssim_similarity_of_same_file_white(self):
         pair = FilePair("files/tests/images/0-0-white.png", "files/tests/images/0-0-white.png")
-        ssim = StructuralSimilarityIndexMeasure()
-        ssim.calculate_similarity(pair)
+        self.ssim(pair)
         assert pair.similarity <= .005, "Same files should return zero"
         assert pair.elapsed > 0, "Elapsed should be bigger than zero"
         assert pair.skipped is False
 
     def test_ssim_similarity_of_white_grey(self):
         pair = FilePair("files/tests/images/0-0-white.png", "files/tests/images/0-2-grey.png")
-        ssim = StructuralSimilarityIndexMeasure()
-        ssim.calculate_similarity(pair)
+        self.ssim(pair)
         assert pair.similarity >= 0, "Files are not same, should be a positive value"
         assert pair.elapsed > 0, "Elapsed should be bigger than zero"
         assert pair.skipped is False
 
     def test_ssim_similarity_of_white_grey_inverse_order(self):
         pair = FilePair("files/tests/images/0-2-grey.png", "files/tests/images/0-0-white.png")
-        ssim = StructuralSimilarityIndexMeasure()
-        ssim.calculate_similarity(pair)
+        self.ssim(pair)
         assert pair.similarity >= 0, "Files are not same, should be a positive value"
         assert pair.elapsed > 0, "Elapsed should be bigger than zero"
         assert pair.skipped is False
 
     def test_ssim_similarity_of_white_black(self):
         pair = FilePair("files/tests/images/0-0-white.png", "files/tests/images/0-1-black.png")
-        ssim = StructuralSimilarityIndexMeasure()
-        ssim.calculate_similarity(pair)
+        self.ssim(pair)
         assert pair.similarity >= .995, f"Files are the opposite, should be one {pair.similarity}"
         assert pair.elapsed > 0, "Elapsed should be bigger than zero"
         assert pair.skipped is False
 
     def test_ssim_similarity_of_white_black_inverse_order(self):
         pair = FilePair("files/tests/images/0-1-black.png", "files/tests/images/0-0-white.png")
-        ssim = StructuralSimilarityIndexMeasure()
-        ssim.calculate_similarity(pair)
+        self.ssim(pair)
         assert pair.similarity >= .995, f"Files are the opposite, should be one {pair.similarity}"
         assert pair.elapsed > 0, "Elapsed should be bigger than zero"
         assert pair.skipped is False
 
     def test_ssim_similarity_big_files_big_wm_cat(self):
         pair = FilePair("files/tests/images/small/cat.png", "files/tests/images/small/cat-wm-big.png")
-        ssim = StructuralSimilarityIndexMeasure()
-        ssim.calculate_similarity(pair)
+        self.ssim(pair)
         assert pair.similarity <= .03
         assert pair.elapsed > 0, "Elapsed should be bigger than zero"
         assert pair.skipped is False
 
     def test_ssim_similarity_big_files_small_wm_cat(self):
         pair = FilePair("files/tests/images/small/cat.png", "files/tests/images/small/cat-wm-small.png")
-        ssim = StructuralSimilarityIndexMeasure()
-        ssim.calculate_similarity(pair)
+        self.ssim(pair)
         assert pair.similarity <= .01
         assert pair.elapsed > 0, "Elapsed should be bigger than zero"
         assert pair.skipped is False
@@ -104,31 +95,29 @@ class TestNRMSESimilarity(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures, if any."""
+        self.nrmse = get_similarity_measurement("nrmse")
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
     def test_nrmse_missing_file_first_argument(self):
         pair = FilePair("no_such_file_exists.png", "files/tests/images/0-0-white.png")
-        nrmse = NormalizedRootMeanSquaredErrorMeasure()
         with self.assertRaises(FileError):
-            nrmse.calculate_similarity(pair)
+            self.nrmse(pair)
         assert pair.skipped is True
         assert pair.similarity == -1.0
         assert pair.elapsed == -1.0
 
     def test_nrmse_similarity_of_white_black(self):
         pair = FilePair("files/tests/images/0-0-white.png", "files/tests/images/0-1-black.png")
-        nrmse = NormalizedRootMeanSquaredErrorMeasure()
-        nrmse.calculate_similarity(pair)
+        self.nrmse(pair)
         assert pair.similarity >= .995, "Files are the opposite, should be one"
         assert pair.elapsed > 0, "Elapsed should be bigger than zero"
         assert pair.skipped is False
 
     def test_nrmse_similarity_of_white_black_inverse_order(self):
         pair = FilePair("files/tests/images/0-1-black.png", "files/tests/images/0-0-white.png")
-        nrmse = NormalizedRootMeanSquaredErrorMeasure()
-        nrmse.calculate_similarity(pair)
+        self.nrmse(pair)
         assert pair.similarity >= .995, "Files are the opposite, should be one"
         assert pair.elapsed > 0, "Elapsed should be bigger than zero"
         assert pair.skipped is False
@@ -138,39 +127,36 @@ class TestMSESimilarity(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures, if any."""
+        self.mse = get_similarity_measurement("mse")
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
     def test_mse_missing_file_first_argument(self):
         pair = FilePair("no_such_file_exists.png", "files/tests/images/0-0-white.png")
-        mse = MeanSquaredErrorMeasure()
         with self.assertRaises(FileError):
-            mse.calculate_similarity(pair)
+            self.mse(pair)
         assert pair.skipped is True
         assert pair.similarity == -1.0
         assert pair.elapsed == -1.0
 
     def test_mse_similarity_of_white_black(self):
         pair = FilePair("files/tests/images/0-0-white.png", "files/tests/images/0-1-black.png")
-        mse = MeanSquaredErrorMeasure()
-        mse.calculate_similarity(pair)
+        self.mse(pair)
 #        assert pair.similarity >= .995, "Files are the opposite, should be one"
         assert pair.elapsed > 0, "Elapsed should be bigger than zero"
         assert pair.skipped is False
 
     def test_mse_similarity_of_white_black_inverse_order(self):
         pair = FilePair("files/tests/images/0-1-black.png", "files/tests/images/0-0-white.png")
-        mse = MeanSquaredErrorMeasure()
-        mse.calculate_similarity(pair)
+        self.mse(pair)
 #        assert pair.similarity >= .995, "Files are the opposite, should be one"
         assert pair.elapsed > 0, "Elapsed should be bigger than zero"
         assert pair.skipped is False
 
     def test_mse_similarity_of_same_file_white(self):
         pair = FilePair("files/tests/images/0-0-white.png", "files/tests/images/0-0-white.png")
-        mse = MeanSquaredErrorMeasure()
-        mse.calculate_similarity(pair)
+        self.mse(pair)
         assert pair.similarity <= .005, "Same files should return zero"
         assert pair.elapsed > 0, "Elapsed should be bigger than zero"
         assert pair.skipped is False
