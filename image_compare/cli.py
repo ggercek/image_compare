54308966625acbd990669993f49333996235684e
Gurcan Gercek <gurcangercek@gmail.com>
2019-08-16 16:05:46 -0400

    Overwrite-output option added to CLI

--- image_compare/cli.py
+++ image_compare/cli.py
@@ -7,18 +7,18 @@ from image_compare import image_compare
 from image_compare.models import Config
 
 
-
 @click.command()
-@click.argument("input_file", type=click.File('r'))
-@click.argument("output_file", type=click.File('w'))
+@click.argument("input_file")
+@click.argument("output_file")
+@click.option("--overwrite-output", is_flag=True, default=False)
 @click.option("--distance", type=click.Choice(["ssim", "nrmse", "mse"]), default="ssim")
 @click.option("--log-level", type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]), default="INFO")
-def main(input_file, output_file, distance, log_level):
+def main(input_file, output_file, overwrite_output, distance, log_level):
     """Console script for image_compare."""
 #    click.echo("Replace this message by putting your code into "
 #               "image_compare.cli.main")
 #    click.echo("See click documentation at http://click.pocoo.org/")
-    config = Config(input_file, output_file, distance, log_level)
+    config = Config(input_file, output_file, overwrite_output, distance, log_level)
     return image_compare.main(config)
 
 
--- image_compare/models.py
+++ image_compare/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 class FilePair:
     def __init__(self, image1, image2, similarity=-1.0, elapsed=-1.0, line_num=-1, skipped=False):
         self.image1 = image1
@@ -14,11 +15,13 @@ class FilePair:
 
 
 class Config:
-    def __init__(self, input_file, output_file, distance="ssim", log_level="INFO"):
+    def __init__(self, input_file, output_file, overwrite_output=False, distance="ssim", log_level="INFO"):
         self.input_file = input_file
         self.output_file = output_file
+        self.overwrite_output = overwrite_output
         self.distance = distance
         self.log_level = log_level
 
     def __repr__(self):
-        return f"Config [input:{self.input_file.name}, output:{self.output_file.name}, distance:{self.distance}, log_level:{self.log_level}]"
+        return f"Config [input:{self.input_file}, output:{self.output_file}, " \
+               f"overwrite_output: {self.overwrite_output}, distance:{self.distance}, log_level:{self.log_level}]"
